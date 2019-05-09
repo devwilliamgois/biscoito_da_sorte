5434718be2a77bdf0d514fd212ad2d449757af61
William Gois <goiswilliam194@gmail.com>
2019-05-08 22:05:54 -0300

    git add -A

--- App.js
+++ App.js
@@ -7,43 +7,63 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View,TouchableOpacity,Image} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+class Botao extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = '';
+    this.style = StyleSheet.create({
+      botao:{
+        color:"#000000",
+        width:250,
+        borderWidth:2,
+        height:50,
+        borderColor:"#000000",
+        borderRadius:25
+      },
+      botaoArea:{
+        flex:1,
+        flexDirection:'row',
+        justifyContent:'center',
+        alignItems:'center'
+      },
+      botaoTexto:{
+        fontSize:14,
+        fontWeight:'bold',
+        textAlign:'center'
+      }
+
+     
+    })
+  }
+
+  render(){
+    return(
+      <TouchableOpacity style={this.style.botao} onPress={this.props.onPress}>
+        <View style={this.style.botaoArea}>
+         <Text style={this.style.botaoTexto}>{this.props.text}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+
+}
+
+
+export default class App extends Component {
+  quebrar_biscoito(){
+    alert('ola mundo');
+  }
+  render() {    
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View >
+      <Image source={require('./images/biscoito.png')}/>
+        <Botao text="Quebrar biscoito" onPress={this.quebrar_biscoito}/>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
