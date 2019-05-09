127ad70010fd8a02c9ed6cb6be0d195c387d288d
William Gois <goiswilliam194@gmail.com>
2019-05-08 22:21:55 -0300

    Finaliando projeto

--- App.js
+++ App.js
@@ -55,15 +55,47 @@ class Botao extends Component{
 
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {"texto":"Frase do dia"}
+    this.frases = ["O importante não é vencer todos os dias, mas lutar sempre.","Maior que a tristeza de não haver vencido é a vergonha de não ter lutado!","É melhor conquistar a si mesmo do que vencer mil batalhas.","Quem ousou conquistar e saiu pra lutar, chega mais longe!","Enquanto houver vontade de lutar haverá esperança de vencer.","Difícil é ganhar um amigo em uma hora; fácil é ofendê-lo em um minuto.","O medo de perder tira a vontade de ganhar.","Aquele que não tem confiança nos outros, não lhes pode ganhar a confiança.","Escolher o seu tempo é ganhar tempo.","Arriscamo-nos a perder quando queremos ganhar demais.","Perder para a razão, sempre é ganhar.","Muitos sabem ganhar dinheiro, mas poucos sabem gastá-lo.","Se você pretende ser rico, pense em economizar tanto quanto em ganhar.","O homem digno ganha para viver; o menos honesto vive para ganhar.","Para ganhar aquilo que vale a pena ter, pode ser necessário perder tudo mais."]
+    this.quebrar_biscoito = this.quebrar_biscoito.bind(this);
+  }
   quebrar_biscoito(){
-    alert('ola mundo');
+    let s = this.state;
+    let r = Math.floor(Math.random () * this.frases.length);
+    s.texto = this.frases[r];
+    this.setState(s);
   }
   render() {    
     return (
-      <View >
-      <Image source={require('./images/biscoito.png')}/>
+      <View style={style.body}>
+      <Image source={require('./images/biscoito.png')} style={style.image}/>
+        <Text style={style.texto}>"{this.state.texto}"</Text>
         <Botao text="Quebrar biscoito" onPress={this.quebrar_biscoito}/>
       </View>
     );
   }
 }
+
+
+const style = StyleSheet.create({
+  body:{
+    paddingTop:20,
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  image:{
+    width:300,
+    height:250
+  },
+  texto:{
+    fontSize:20,
+    color:"#FF0000",
+    margin:30,
+    fontStyle:'italic',
+    textAlign:'center'
+  }
+})
